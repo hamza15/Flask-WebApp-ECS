cc8c92ea5d9340daa1ec357a2114a4109cb48668
Sardar Hamza Naqi <sardan5@vt.edu>
2019-01-29 00:19:55 -0500

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 import sys
 
 def load(file):
-    """Open a text file & turn contents into a list of lowercase strings."""
+    #Open a text file & turn contents into a list of lowercase strings
     with open(file) as in_file:
         loaded_txt = in_file.read().strip().split('\n')
         loaded_txt = [x.lower() for x in loaded_txt]
@@ -13,17 +13,23 @@ def load(file):
 
 word_list = load('file.txt')
 
+#Get request to load index.html
+#POST request to load solver.html
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/anagrams/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
+        #empty list to store answers in
         anagram_list = []
+        #Get user input from text form
         value = request.form['letters']
+        #sort the letters and convert to lowercase
         value = ''.join(sorted(value))
         value = value.lower()
         value_sorted = sorted(value)
+        #Run through the dictionary file of sorted words in list format and compare with sorted user input 
         for word in word_list:
             word = word.lower()
             if word != value:
@@ -31,6 +37,7 @@ def index():
                     anagram_list.append(word)
         return render_template('solver.html', myWords = anagram_list)  
 
+#Run the app on port 80
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port=80)
